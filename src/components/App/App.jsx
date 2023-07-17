a5327864677dcec3cc7b6c24104a55021cc22a76
valentynaLogosha <cristianyshka@gmail.com>
2023-07-17 21:36:56 +0300

    change

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,37 +1,16 @@
-
-import user from 'user.json';
-import { Profile } from "components/Profile";
-
-import data from 'data.json';
-import { Statistics } from "../Statistics";
-
-import friends from 'friends.json'
-import { FriendList } from "../FriendList";
-
-import transactions from 'transactions.json'
-import { TransactionHistory } from "../TransactionHistory";
-
-
-import { Container } from "./App.styled";
-
 export const App = () => {
   return (
-    <Container >
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        followers={user.stats.followers}
-        views={user.stats.views}
-        likes={user.stats.likes}
-      />
-      <Statistics
-        title="UPLOAD STATS"
-        stats={data}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </Container>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
   );
 };
